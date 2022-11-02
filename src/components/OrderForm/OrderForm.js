e06b6b981554e3f4974cfa0b56f9ed89e10bbcf4
Matt Bowen <mattbowen03@gmail.com>
2022-11-02 09:22:10 -0500

    form validation

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -3,63 +3,141 @@ import { useContext } from "react";
 import React from "react";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
+import useInput from "../hooks/use-input";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
 
 export const OrderForm = (props) => {
+  const {
+    value: firstNameValue,
+    isValid: firstNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+
   const cartCtx = useContext(CartContext);
-
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems = cartCtx.items.length > 0;
 
-  const orderForm = (
-    <form>
-      <div className={classes["control-group"]}>
-        <div className={classes["form-control"]}>
-          <label htmlFor='firstName'> First Name</label>
-          <input id='firstName' type='text' />
-        </div>
-        <div className={classes["form-control"]}>
-          <label htmlFor='lastName'> Last Name</label>
-          <input id='lastName' type='text' />
-        </div>
-        <div className={classes["form-control"]}>
-          <label htmlFor='email'> Email</label>
-          <input id='email' type='email' />
-        </div>
-        <div className={classes["form-control"]}>
-          <label htmlFor='street'> Street</label>
-          <input id='street' type='street' />
-        </div>
-        <div className={classes["form-control"]}>
-          <label htmlFor='city'> city</label>
-          <input id='city' type='city' />
-        </div>
-        <div className={classes["form-control"]}>
-          <label htmlFor='zip'> zip</label>
-          <input id='zip' type='zip' />
-        </div>
-        <div className={classes["form-control"]}>
-          <label htmlFor='state'> State</label>
-          <input id='state' type='state' />
-        </div>
-      </div>
-    </form>
-  );
+  let formIsValid = false;
+
+  if (firstNameIsValid) {
+    formIsValid = true;
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (!formIsValid) {
+      return;
+    }
+    console.log(firstNameValue);
+    console.log(lastNameValue);
+    console.log(emailValue);
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+    // props.onClose();
+  };
+
+  const firstNameClasses = firstNameHasError
+    ? `${classes["form-control"]} ${classes["invalid"]}`
+    : classes["form-control"];
+
+  const lastNameClasses = lastNameHasError
+    ? `${classes["form-control"]} ${classes["invalid"]}`
+    : classes["form-control"];
+
+  const emailClasses = emailHasError
+    ? `${classes["form-control"]} ${classes["invalid"]}`
+    : classes["form-control"];
 
   return (
     <Modal onClose={props.onClose}>
-      {orderForm}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      <div className={classes.actions}>
-        <button
-          className={classes["button--alt"]}
-          onClick={props.onReturnToCart}>
-          Cart
-        </button>
-        <button className={classes.button}>Place Order</button>
-      </div>
+      <form onSubmit={submitHandler}>
+        <div className={classes["control-group"]}>
+          <div className={firstNameClasses}>
+            <label htmlFor='firstName'> First Name</label>
+            <input
+              value={firstNameValue}
+              onChange={firstNameChangeHandler}
+              onBlur={firstNameBlurHandler}
+              id='firstName'
+              type='text'
+            />
+            {firstNameHasError && (
+              <p className={classes["error-text"]}>
+                Please enter a first name.
+              </p>
+            )}
+          </div>
+          <div className={lastNameClasses}>
+            <label htmlFor='lastName'> Last Name</label>
+            <input
+              value={lastNameValue}
+              onChange={lastNameChangeHandler}
+              onBlur={lastNameBlurHandler}
+              id='lastName'
+              type='text'
+            />
+            {lastNameHasError && (
+              <p className={classes["error-text"]}>Please enter a last name.</p>
+            )}
+          </div>
+          <div className={emailClasses}>
+            <label htmlFor='email'> Email</label>
+            <input
+              value={emailValue}
+              onChange={emailChangeHandler}
+              onBlur={emailBlurHandler}
+              id='email'
+              type='email'
+            />
+            {emailHasError && (
+              <p className={classes["error-text"]}>
+                Please enter a valid email address.
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className={classes.total}>
+          <span>Total Amount</span>
+          <span>{totalAmount}</span>
+        </div>
+        <div className={classes.actions}>
+          <button
+            className={classes["button--alt"]}
+            onClick={props.onReturnToCart}>
+            Cart
+          </button>
+          <button
+            disabled={!formIsValid}
+            type='submit'
+            className={classes.button}>
+            Place Order
+          </button>
+        </div>
+      </form>
     </Modal>
   );
 };
--- src/components/hooks/use-input.js
+++ src/components/hooks/use-input.js
@@ -1,7 +1,33 @@
 import { useState } from "react";
 
-const useInput = () => {
-  const [value, setEnteredValue] = useState("");
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const isValid = validateValue(enteredValue);
+  const hasError = !isValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    valueChangeHandler,
+    isValid,
+    hasError,
+    inputBlurHandler,
+    reset,
+  };
 };
 
 export default useInput;
